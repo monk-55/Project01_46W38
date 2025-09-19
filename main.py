47ff218d6da21662ebcbbf1d20e7ef6d761d9fc7
monk-55 <liv.tomkins@gmail.com>
2025-09-19 12:51:01 +0200

    Project01 - setting up basic format of script ready for editing

---
+++ main.py
@@ -0,0 +1,46 @@
+
+"""
+task details:
+In this project, you need to write a function to calculate the power output by implementing the model defined in the last section. Your function need to fullfil the following requirements:
+
+Have a meaningful and proper name.
+Takes the following inputs (each with proper variable name):
+wind speed (we assume it will be a float number),
+rated power, with a default value: 15,
+cut-in wind speed, with a default value: 3,
+rated wind speed, with a default value: 11,
+cut-out wind speed, with a default value: 25,
+interpolation option, a string to define the interpolation method, can only be "linear" or "cubic", with a default value: "linear".
+Returns the computed power output using the model and the inputs.
+Have proper docstring and several (at least 3) comments to explain the function.
+Include error handling by raising proper error when the user gives an invalid input for the string defining the interpolation option.
+You should also write the main script to run your function for at least one example. These can be done in one script that looks like the following:
+"""
+#Creating the function
+
+def power_output(wind_speed, rated_power, cut_in, rated_wind_speed, cut_out):
+    """
+    Docstring here.
+    """
+    # Add comment if needed.
+    result = x +  y # Add comment if needed.
+
+    return result
+
+if __name__ == '__main__':
+    # Write the main script to use the function here:
+    wind_speed = 5
+    rated_power = 15
+    cut_in = 3
+    rated_wind_speed = 11
+    cut_out = 25
+
+
+    # Add comments to explain if needed.
+    power = power_output(x, y)
+    print(f'x + y = {z}')  # Add comment when needed
+
+
+
+
+
